0bbc23f23ab652acea4caeefda1542b6896a88ca
Rohit Saroha <rohitsaroha125@gmail.com>
2023-10-30 15:02:08 +0530

    FIX: Image tag fixes for pagespeed

--- frontend/components/Rightside.tsx
+++ frontend/components/Rightside.tsx
@@ -9,18 +9,24 @@ const Rightside: NextPage = () => {
       <Image
         className={styles.happyBeardedYoungManLooksIcon}
         alt=""
+        width={447}
+        height={596}
         src="/happybeardedyoungmanlookswithjoyfulexpressionhasfriendlysmilewearsyellowsweaterredhat-1@2x.png"
       />
-      <Image className={styles.rightsideItem} alt="" src="/vector-2.svg" />
+      <Image className={styles.rightsideItem} width={150}
+      height={335} alt="" src="/vector-2.svg" />
       <div className={styles.rightsideInner} />
-      <Image className={styles.ellipseIcon} alt="" src="/ellipse-2@2x.png" />
+      <Image className={styles.ellipseIcon} width={103}
+      height={103} alt="" src="/ellipse-2@2x.png" />
       <b className={styles.received}>Received!</b>
       <div className={styles.ellipseDiv} />
       <div className={styles.rightsideChild1} />
-      <Image className={styles.frameIcon} alt="" src="/frame-4.svg" />
+      <Image className={styles.frameIcon} width={128}
+      height={128} alt="" src="/frame-4.svg" />
       <div className={styles.ellipseParent}>
         <div className={styles.frameChild} />
-        <Image className={styles.checkIcon} alt="" src="/check.svg" />
+        <Image className={styles.checkIcon} width={27}
+          height={27.6} alt="" src="/check.svg" />
         <div className={styles.youJustSent}>
           You just sent $20.00 to Sofia Alexander
         </div>
