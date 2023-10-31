fcf82ffd396335753cfd183614957a65ee30152d
Rohit Saroha <rohitsaroha125@gmail.com>
2023-10-31 17:10:10 +0530

    SETUP: Express setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,21 +1,38 @@
+import 'dotenv/config'
 import { AppDataSource } from "./data-source"
 import { User } from "./entity/User"
+import * as express from "express"
+import { Request, Response } from "express"
+
+require('dotenv').config()
+
+const app = express()
+const port = 5000 || process.env.PORT
+app.use(express.json())
 
 AppDataSource.initialize().then(async () => {
+    // console.log("Inserting a new user into the database...")
+    // const user = new User()
+    // user.firstName = "Timber"
+    // user.lastName = "Saw"
+    // user.email = "rohit@yopmail.com"
+    // user.password = "123456"
+    // await AppDataSource.manager.save(user)
+    // console.log("Saved a new user with id: " + user.id)
 
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.email = "rohit@yopmail.com"
-    user.password = "123456"
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
+    // console.log("Loading users from the database...")
+    // const users = await AppDataSource.manager.find(User)
+    // console.log("Loaded users: ", users)
 
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
-    console.log("Here you can setup and run express / fastify / any other framework.")
-
+    // console.log("Here you can setup and run express / fastify / any other framework.")
+    console.log("Connected with database")
 }).catch(error => console.log(error))
+
+app.get("/users", function (req: Request, res: Response) {
+    // here we will have logic to return all users
+    res.send('Hello world')
+})
+
+app.listen(port,() => {
+    console.log(`Server started on ${port}`)
+})
